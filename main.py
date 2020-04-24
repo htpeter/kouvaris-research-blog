7d74943b25f299513fcfd1cdbaaa011bb581956a
htpeter <kouvaris.peter@gmail.com>
2020-04-24 07:04:09 -0400

    Revert "roam push"
    This reverts commit be23d460be3487656ba9352e0d12e570c6ced964.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from backend.serve import app
 
-app.run(debug=True, use_reloader=True)
+app.run(debug=False)
 
 # https://www.freecodecamp.org/news/how-to-build-a-web-application-using-flask-and-deploy-it-to-the-cloud-3551c985e492/
 # https://getbootstrap.com/docs/3.4/customize/
