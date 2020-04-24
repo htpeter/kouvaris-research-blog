1b0542bee0454db8c751fd31e6c674f40760934b
htpeter <kouvaris.peter@gmail.com>
2020-04-24 07:04:24 -0400

    Revert "landing page for frans"
    This reverts commit 8fb06b40268479cea5bfd9a3d6c9e1ded84808b8.

--- backend/serve.py
+++ backend/serve.py
@@ -2,25 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-ROOT_PAGE = 'research-development'
 
 @app.route("/")
 def homepage():
-    return render_template("./roam_notes/{0}-md.html".format(ROOT_PAGE))
-
-@app.route("/r/<slug>")
-def roam_template(**kwargs):
-    # print(kwargs)
-    return render_template("./roam_notes/{slug}-md.html".format(**kwargs))
+    return render_template("./homepage.html")
 
 
 @app.route("/posts/model_monitoring")
 def model_monitoring_post():
     return render_template("./posts/model_monitoring.html")
-
-@app.route("/landing")
-def landing_page():
-    return render_template("./landing/friend_fb_landing_page.html")
 
 
 @app.route("/posts/tiered_open_education")
