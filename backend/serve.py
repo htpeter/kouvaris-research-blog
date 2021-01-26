36a4eaf9666676fe62bffcd4c2b53cfe716eaabf
Peter Kouvaris <kouvaris.peter@gmail.com>
2021-01-26 18:36:59 -0500

    refactoring general flow to not use roam static site generator

--- backend/serve.py
+++ backend/serve.py
@@ -2,30 +2,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-ROOT_PAGE = 'research-development'
 
 @app.route("/")
 def homepage():
-    return render_template("./roam_notes/{0}-md.html".format(ROOT_PAGE))
-
-@app.route("/r/<slug>")
-def roam_template(**kwargs):
-    # print(kwargs)
-    return render_template("./roam_notes/{slug}-md.html".format(**kwargs))
-
-
-@app.route("/posts/model_monitoring")
-def model_monitoring_post():
-    return render_template("./posts/model_monitoring.html")
-
-@app.route("/landing")
-def landing_page():
-    return render_template("./landing/friend_fb_landing_page.html")
-
-
-@app.route("/posts/tiered_open_education")
-def tiered_open_education():
-    return render_template("./posts/tiered_open_education.html")
+    template_variables = {
+        "page_nme": "@/",
+        "pages": [
+            {
+                "page_name": "test page 1",
+                "content": """
+        Twoooooooooooo
+        """,
+            },
+            {"page_name": "test page 2"},
+        ],
+    }
+    return render_template("./homepage.html", **template_variables)
 
 
 if __name__ == "__main__":
